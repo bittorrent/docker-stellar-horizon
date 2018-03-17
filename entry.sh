38d4b37861caa035a874c1b4fd7ecfd8311d52a7
Marc Richards <talawahtech@users.noreply.github.com>
2018-03-17 14:19:57 -0700

    Make DATA_DIR optional and support passing in DB_PORT (optional)

--- entry.sh
+++ entry.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -ue
 
+DATA=${DATA_DIR:-/data}
+PORT=${DB_PORT:-5432}
+
 # Wait for postgres
 export PGPASSWORD=${DB_PASS}
-while ! psql -h "${DB_HOST}" -U "${DB_USER}" -c 'select 1' ${DB_NAME} &> /dev/null ; do
+while ! psql -h "${DB_HOST}" -U "${DB_USER}" -p "${PORT}" -c 'select 1' "${DB_NAME}" &> /dev/null ; do
   echo "Waiting for postgres to be available..."
   sleep 1
 done
@@ -20,11 +23,11 @@ done
 
 
 # Check if DB needs to be initialized
-DB_INITIALIZED_MARKER="${DATA_DIR}/horizon/.db-initialized"
+DB_INITIALIZED_MARKER="${DATA}/horizon/.db-initialized"
 if [ ! -f ${DB_INITIALIZED_MARKER} ]; then
     echo "initializing horizon db..."
     horizon db init
-    mkdir -p ${DATA_DIR}/horizon
+    mkdir -p ${DATA}/horizon
     touch ${DB_INITIALIZED_MARKER}
 fi
 
