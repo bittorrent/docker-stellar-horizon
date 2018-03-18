9c50b9d9e8967bbb7b02665d51b55ff208e5fb39
Marc Richards <talawahtech@users.noreply.github.com>
2018-03-17 21:55:47 -0700

    Use better initialization to avoid overwriting other env vars

--- entry.sh
+++ entry.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 set -ue
 
-DATA=${DATA_DIR:-/data}
-PORT=${DB_PORT:-5432}
+: ${DATA_DIR:=/data}
+: ${DB_PORT:=5432}
 
 # Wait for postgres
 export PGPASSWORD=${DB_PASS}
-while ! psql -h "${DB_HOST}" -U "${DB_USER}" -p "${PORT}" -c 'select 1' "${DB_NAME}" &> /dev/null ; do
+while ! psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -c 'select 1' "${DB_NAME}" &> /dev/null ; do
   echo "Waiting for postgres to be available..."
-  sleep 1
+  sleep 5
 done
 
 # Wait 3 more seconds for postgres to restart itself
-sleep 3
+sleep 5
 
 
 # Wait for stellar-core
@@ -23,11 +23,11 @@ done
 
 
 # Check if DB needs to be initialized
-DB_INITIALIZED_MARKER="${DATA}/horizon/.db-initialized"
+DB_INITIALIZED_MARKER="${DATA_DIR}/horizon/.db-initialized"
 if [ ! -f ${DB_INITIALIZED_MARKER} ]; then
     echo "initializing horizon db..."
     horizon db init
-    mkdir -p ${DATA}/horizon
+    mkdir -p ${DATA_DIR}/horizon
     touch ${DB_INITIALIZED_MARKER}
 fi
 
